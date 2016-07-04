a3c09834b0d60dff69851325832fcad6a982c52e
SublimeAllan <allan@sublimeskinz.com>
2016-07-04 13:49:39 +0200

    Nav / add coloration on scroll

--- src/js/controller.js
+++ src/js/controller.js
@@ -2,6 +2,7 @@ var Controller = (function(){
   var Controller = function(view){
     this.view = view
     this.animateLanding()
+    this.colorNavbar()
   }
 
   Controller.prototype = {
@@ -9,6 +10,11 @@ var Controller = (function(){
       this.view.animateLanding(
         document.querySelector('.Landing_title'),
         document.querySelector('.Landing_subtitle')
+      )
+    },
+    colorNavbar: function() {
+      this.view.colorNavbar(
+        document.querySelector('.Nav')
       )
     }
   }
--- src/js/view.js
+++ src/js/view.js
@@ -4,6 +4,14 @@ var View = (function(){
 
   View.prototype = {
     animateLanding: function(title, subtitle) {
+    },
+
+    colorNavbar: function(navbar) {
+      var scrollMagicController = new ScrollMagic.Controller()
+      var scene = new ScrollMagic.Scene({triggerElement: '.Continue'})
+                                      .offset(window.innerHeight/2 - 60)
+                                      .setClassToggle('.Nav', 'colored')
+                                      .addTo(scrollMagicController)
     }
   }
   return View
