419a8c7badda12885fb9b53b3da2bfe7b6b48161
AllanMichay <allan.michay@gmail.com>
2016-07-06 01:10:27 +0200

    Doc

--- README.md
+++ README.md
@@ -21,10 +21,46 @@ npm i
 
 Tweenmax allows to create easy animations and timelines. In this course, we will use a lot of timeline because they're easily modified.
 
-Create a timeline :
+### Create a timeline :
 
 ```javascript
 var tween = new TimelineMax()
 ```
 
 Using a TimelineMax instance, we can access to all functionnalities and all easing functions. You can use TimelineLite if you want it to be lighter.
+
+### Timeline possibilities
+
+You can use `from`, `to` or `fromTo` to define whether you want to change the beginning state of the animated element, the end one or both.
+
+## MagicScroll
+
+We can easily use MagicScroll and Tweenmax together. We just need to add the plugin gsap.animation.
+
+### Controller
+
+MagicScroll needs a Controller, it will contain every scene of ou scroll.
+
+You can create it using :
+
+```javascript
+var controller = new ScrollMagic.Controller()
+```
+
+### Scene
+
+A Scene is the screen part that needs to be visible for the rest of our script to be launched. It is defined with a `triggerElement`
+
+You can create it using :
+
+```javascript
+var scene = new ScrollMagic.Scene({triggerElement: '.mySelector'})
+```
+
+
+## MagicScroll and Tweenmax
+
+Just create your Tweenmax animation and send it to the scene using
+```javascript
+myScene.setTween(myTween)
+```
--- src/js/view.js
+++ src/js/view.js
@@ -4,6 +4,7 @@ var View = (function(){
 
   View.prototype = {
     animateLanding: function(title, subtitle) {
+      // On crée notre timeline. for = on défini l'état de départ de l'objet à animer. L'état final est défini en CSS
       var tl = new TimelineMax()
       tl.from(title, 0.5, {opacity: 0, y: 30, ease: Power4.easeOut}, '+=0.5')
         .from(subtitle, 0.5, {opacity: 0, y: 30, ease: Power4.easeOut})
@@ -18,11 +19,17 @@ var View = (function(){
     },
 
     animateKeepScrolling: function(title, subtitle) {
+      // On crée notre timeline. for = on défini l'état de départ de l'objet à animer. L'état final est défini en CSS
       var tl = new TimelineMax()
       tl.from(title, 0.5, {opacity: 0, y: 30, ease: Power4.easeOut}, '+=0.5')
         .from(subtitle, 0.5, {opacity: 0, y: 30, ease: Power4.easeOut})
 
+      // On crée un controller pour contenir notre scene
       var controller = new ScrollMagic.Controller()
+      // On crée notre scene.
+      // triggerElement = l'element qui doit arriver au milieu de l'écran pour lancer le reste
+      // setTween sert à lier notre animation TweenMax au scroll
+      // reverse false = ne joue pas mon animation à l'envers quand je remonte
       var scene = new ScrollMagic.Scene({triggerElement: '.Continue'})
                                         .setTween(tl)
                                         .reverse(false)
@@ -34,6 +41,7 @@ var View = (function(){
       tl.fromTo(leftImage, 0.5, {y: -70}, {y: 0})
         .fromTo(rightImage, 0.5, {y: -70}, {y: 0}, '-=0.5')
 
+      // Duration  = nombre de pixel sur lequel le scroll lancera l'animation
       var controller = new ScrollMagic.Controller()
       var scene = new ScrollMagic.Scene({triggerElement: '.AboutMe'})
                                         .setTween(tl)
@@ -42,6 +50,7 @@ var View = (function(){
     },
 
     staggerProjects: function(elements) {
+      // Stagger = animation sur une liste d'élément
       var tween = TweenMax.staggerFrom(elements, 0.5, {scale: 0, ease: Back.easeOut}, 0.5)
 
       var controller = new ScrollMagic.Controller()
@@ -69,6 +78,7 @@ var View = (function(){
       for(var i = 0; i < lg; i++) {
         var element = elements[i]
         var length = lengths[i]
+        // On défini que la bordure est en tiret et on dit que l'espace entre les tirets = la longueur complète de la bordure. Ca permet de tout cacher.
         tl.fromTo(element, 0.5, {strokeDashoffset: length, strokeDasharray: length}, {strokeDashoffset: 0}, '-=0.2')
           .fromTo(element, 0.1, {fill: 'transparent'}, {fill: '#FFFFFF'})
       }
