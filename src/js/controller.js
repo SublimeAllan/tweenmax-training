85c00f8558aaa9bf2617a74d3fb5c1879e570505
SublimeAllan <allan@sublimeskinz.com>
2016-07-05 14:44:58 +0200

    Every animation done

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,25 +1,71 @@
 var Controller = (function(){
   var Controller = function(view){
     this.view = view
-    this.animateLanding()
     this.colorNavbar()
+    this.animateLanding()
+    this.animateKeepScrolling()
+    this.parallaxAboutMe()
+    this.staggerProjects()
     this.writeWord()
   }
 
   Controller.prototype = {
+    colorNavbar: function() {
+      this.view.colorNavbar(
+        document.querySelector('.Nav')
+      )
+    },
     animateLanding: function() {
       this.view.animateLanding(
         document.querySelector('.Landing_title'),
         document.querySelector('.Landing_subtitle')
       )
     },
-    colorNavbar: function() {
-      this.view.colorNavbar(
-        document.querySelector('.Nav')
+    animateKeepScrolling: function() {
+      this.view.animateKeepScrolling(
+        document.querySelector('.Continue_title'),
+        document.querySelector('.Continue_subtitle')
       )
     },
+    parallaxAboutMe: function() {
+      this.view.parallaxAboutMe(
+        document.querySelector('.AboutMe_image--left'),
+        document.querySelector('.AboutMe_image--right'),
+        document.querySelector('.AboutMe').clientHeight
+      )
+    },
+    staggerProjects: function() {
+      this.view.staggerProjects(document.querySelectorAll('.Projects_card'))
+    },
     writeWord: function() {
-      this.view.writeWord()
+      this.view.writeWord(
+        [
+          document.querySelector('#s'),
+          document.querySelector('#a'),
+          document.querySelector('#y'),
+          document.querySelector('#h'),
+          document.querySelector('#e'),
+          document.querySelector('#l'),
+          document.querySelector('#l2'),
+          document.querySelector('#o'),
+          document.querySelector('#bang')
+        ],
+        [
+          this.preparePath(document.querySelector('#s')),
+          this.preparePath(document.querySelector('#a')),
+          this.preparePath(document.querySelector('#y')),
+          this.preparePath(document.querySelector('#h')),
+          this.preparePath(document.querySelector('#e')),
+          this.preparePath(document.querySelector('#l')),
+          this.preparePath(document.querySelector('#l2')),
+          this.preparePath(document.querySelector('#o')),
+          this.preparePath(document.querySelector('#bang'))
+        ],
+        document.querySelector('.Contact_textarea').offsetTop - document.querySelector('.Contact').offsetTop
+      )
+    },
+    preparePath: function($el) {
+      return lineLength = $el.getTotalLength();
     }
   }
   return Controller
--- src/js/view.js
+++ src/js/view.js
@@ -4,6 +4,9 @@ var View = (function(){
 
   View.prototype = {
     animateLanding: function(title, subtitle) {
+      var tl = new TimelineMax()
+      tl.from(title, 0.5, {opacity: 0, y: 30, ease: Power4.easeOut}, '+=0.5')
+        .from(subtitle, 0.5, {opacity: 0, y: 30, ease: Power4.easeOut})
     },
 
     colorNavbar: function(navbar) {
@@ -14,11 +17,63 @@ var View = (function(){
                                       .addTo(scrollMagicController)
     },
 
-    writeWord: function() {
+    animateKeepScrolling: function(title, subtitle) {
+      var tl = new TimelineMax()
+      tl.from(title, 0.5, {opacity: 0, y: 30, ease: Power4.easeOut}, '+=0.5')
+        .from(subtitle, 0.5, {opacity: 0, y: 30, ease: Power4.easeOut})
+
+      var controller = new ScrollMagic.Controller()
+      var scene = new ScrollMagic.Scene({triggerElement: '.Continue'})
+                                        .setTween(tl)
+                                        .reverse(false)
+                                        .addTo(controller)
     },
 
-    preparePath: function($el) {
-      return lineLength = $el.getTotalLength();
+    parallaxAboutMe: function(leftImage, rightImage, duration) {
+      var tl = new TimelineMax()
+      tl.fromTo(leftImage, 0.5, {y: -70}, {y: 0})
+        .fromTo(rightImage, 0.5, {y: -70}, {y: 0}, '-=0.5')
+
+      var controller = new ScrollMagic.Controller()
+      var scene = new ScrollMagic.Scene({triggerElement: '.AboutMe'})
+                                        .setTween(tl)
+                                        .duration(duration)
+                                        .addTo(controller)
+    },
+
+    staggerProjects: function(elements) {
+      var tween = TweenMax.staggerFrom(elements, 0.5, {scale: 0, ease: Back.easeOut}, 0.5)
+
+      var controller = new ScrollMagic.Controller()
+      var scene = new ScrollMagic.Scene({triggerElement: '.Projects'})
+                                        .setTween(tween)
+                                        .reverse(false)
+                                        .addTo(controller)
+    },
+
+    writeWord: function(elements, lengths, duration) {
+      var tl = this.prepareTimeline(elements, lengths)
+
+      var controller = new ScrollMagic.Controller()
+      var scene = new ScrollMagic.Scene({triggerElement: '.Contact'})
+                                        .setTween(tl)
+                                        .reverse(false)
+                                        .addTo(controller)
+    },
+
+    prepareTimeline: function(elements, lengths) {
+      var tl = new TimelineMax()
+
+      var lg = elements.length;
+
+      for(var i = 0; i < lg; i++) {
+        var element = elements[i]
+        var length = lengths[i]
+        tl.fromTo(element, 0.5, {strokeDashoffset: length, strokeDasharray: length}, {strokeDashoffset: 0}, '-=0.2')
+          .fromTo(element, 0.1, {fill: 'transparent'}, {fill: '#FFFFFF'})
+      }
+
+      return tl
     }
 
   }
